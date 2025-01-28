453982f40afbf107c9cd9b4c28fbc8f934852825
Paul Ramos <palvarez-2023238@kinal.edu.gt>
2025-01-28 17:34:39 -0600

    Add: DDoS protection using express-rate-limit among other things.

--- configs/server.js
+++ configs/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import apiLimiter from "../src/middlewares/validate-petition-amount.js"
 
 import { dbConnection } from './mongo.js'
 import authRoutes from "../src/auth/auth.routes.js"
@@ -13,6 +14,7 @@ const middlewares = (app) => {
     app.use(cors())
     app.use(helmet())
     app.use(morgan('dev'))
+    app.use(apiLimiter)
 }
 
 const routes = (app) => {
--- src/middlewares/check-validator.js
+++ src/middlewares/check-validator.js
@@ -3,6 +3,9 @@ import { existentEmail } from "../helpers/db-validators.js";
 import { validateFields } from "./validate-fields.js";
 
 export const registerValidator = [
+    check("name", "El nombre es obligatorio.").not().isEmpty(),
+    check("email", "Ingrese un e-mail válido.").isEmail(),
+    check("password").isStrongPassword(),
     check("email").custom(existentEmail),
     validateFields
 ]
